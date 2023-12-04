b1c44090131d41d51dbd961d4ad46271fcc4e91d
Abel N. Nieva <931497+abelnnieva@users.noreply.github.com>
2023-12-04 17:19:05 +0000

    tasks and task-item components

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { Task } from 'src/app/task';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css'],
+})
+export class TaskItemComponent {
+  @Input() task?: Task;
+  faTimes = faTimes;
+}
